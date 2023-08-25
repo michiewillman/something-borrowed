baebb7f3dbae1159d6604b0de604a1f020333183
Michie <contact@michiewillman.com>
2023-08-24 22:08:56 -0700

    update cloudinary keys

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const expbs = require('express-handlebars');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-require('dotenv').config();
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const expbs = require("express-handlebars");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+require("dotenv").config();
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require("cloudinary").v2;
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 cloudinary.config({
-  cloud_name: 'du1rn35uq',
+  cloud_name: "dnmefbufn",
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true
-})
+  secure: true,
+});
 const hbs = expbs.create({});
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-
 
 // Set up cookie session
 const sess = {
@@ -47,5 +46,5 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`Server is listening at PORT: ${PORT}`);
-})
+  });
 });
